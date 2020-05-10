eaf8d210b30ca9bca1121a21c69af9292aa317f1
Administrator <1055271769@qq.com>
2020-05-10 15:43:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pytorch-YOLOv4
+
+[原地址](https://github.com/Tianxiaomo/pytorch-YOLOv4)
+
+- 推理部分 正在加注释
+- 训练部分原仓库未更新完毕，正在跟进
+- 源码接近yolov3, 看过我之前注释过的yolov3就很容易理解
+- 原仓库基于yolov3改的，将 darknet配置文件 转化为pytorch模型并加载Draknet预训练权重
+
+
+
+
+```
+@article{yolov4,
+  title={YOLOv4: YOLOv4: Optimal Speed and Accuracy of Object Detection},
+  author={Alexey Bochkovskiy, Chien-Yao Wang, Hong-Yuan Mark Liao},
+  journal = {arXiv},
+  year={2020}
+}
+```
---
+++ demo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+'''
+@Time          : 20/04/25 15:49
+@Author        : huguanghao
+@File          : demo.py
+@Noice         :
+@Modificattion :
+    @Author    :
+    @Time      :
+    @Detail    :
+
+'''
+from utils.utils import *
+from tool.darknet2pytorch import Darknet
+def detect(cfgfile, weightfile, imgfile):
+    # 根据 配置文件 初始化网络
+    m = Darknet(cfgfile)
+    # 打印网络
+    m.print_network()
+    # 加载 模型权重
+    m.load_weights(weightfile)
+    print('Loading weights from %s... Done!' % (weightfile))
+
+    # 默认使用 coco类别
+    namesfile = 'data/coco.names'
+    # 默认CPU，不使用cuda
+    use_cuda = 0
+    if use_cuda:
+        m.cuda()
+    # 读取 测试图片并转为 RGB通道
+    img = Image.open(imgfile).convert('RGB')
+    # 测试图像 调整尺度，以便输入网络
+    sized = img.resize((m.width, m.height))
+    # 统计第二次运行结果 的时间更稳定，更具代表性？
+    for i in range(2):
+        start = time.time()
+        boxes = do_detect(m, sized, 0.5, 0.4, use_cuda)
+        finish = time.time()
+        if i == 1:
+            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
+    # 加载类别名称，为 bbox打类别标签
+    class_names = load_class_names(namesfile)
+    # 将bbox及类别 绘制到 测试图像并保存
+    plot_boxes(img, boxes, 'predictions.jpg', class_names)
+
+if __name__ == '__main__':
+    cfgfile='cfg/yolov4.cfg' # 网络框架配置文件
+    weightfile='yolov4.weights' # 预训练权重
+    imgfile='bo.jpg' # 测试图像路径
+    # 开始检测
+    detect(cfgfile, weightfile, imgfile)
+
